e71459665dfc0d64e73ac90f6565387864899d3d
Валерий Val <valerich_rus@mail.ru>
2026-02-02 15:22:45 +0300

    Fixes

--- articles/views.py
+++ articles/views.py
@@ -6,7 +6,7 @@ from articles.models import Article
 def articles_list(request):
 
     template = 'articles/news.html'
-    object_list = Article.objects.all()
+    object_list = Article.objects.order_by('-published_at')
     context = {'object_list': object_list}
 
     # используйте этот параметр для упорядочивания результатов
